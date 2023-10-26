37a25d5f5bc550d6b9fca940665eea270fe93680
Prem-Pandey <dev.premrpandey@gmail.com>
2023-10-26 17:18:06 +0530

    using Async Await

---
+++ asyntAwait.js
@@ -0,0 +1,50 @@
+console.log('person1 shows ticket');
+console.log('person2 shows ticket');
+
+const preMovie = async () => {
+
+  const person3PromiseToShowTicketWhenWifeArrives = new Promise((resolve, reject) => {
+    setTimeout(() => resolve('ticket'), 3000);
+  });
+  const getPopcorn =  new Promise((resolve, reject) => {
+		setTimeout(() => resolve('popcorn'), 3000);
+  });
+  
+  const addButter =  new Promise((resolve, reject) => {
+		setTimeout(() => resolve('butter'), 3000);
+  });
+
+  const bringColdDrink = new Promise((resoleve, reject) =>
+  {
+        setTimeout(()=> resoleve('coldDrink'), 3000)
+  })
+
+  let ticket = await person3PromiseToShowTicketWhenWifeArrives;
+
+  console.log(`got the ${ticket}`);
+  console.log(`Husband:we should go in now`);
+  console.log(`Wife: "i am hungry"`);
+  
+  let popcorn = await getPopcorn;
+  console.log(`Husband: here is ${popcorn}`);
+	console.log(`Husband:we should go in now`);
+  console.log(`Wife: "I dont like popcorn without butter!"`);
+  
+  let butter = await addButter;
+  console.log(`added ${butter}`);
+
+  let coldDrink = await bringColdDrink;
+  console.log(`Husband: do you need anything else`)
+  console.log(`wife: what about cold drinks`)
+  console.log(`Husband: ok darling wait i'll bring you cold drinks`)
+  console.log(`Husband: here is your cold drink, anything else darling`);
+  console.log(`Wife: lets go we are going to miss the preivew`);
+  console.log(`Husband: thanks for the reminder *grin*`);
+  
+  return ticket;
+  
+};
+
+preMovie().then((t) => console.log(`person4 shows ${t}`));
+
+console.log('person4 shows ticket');
